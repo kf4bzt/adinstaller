e587ca4d826956e556d561a6765c459b6832b08c
kf4bzt <kf4bzt@gmail.com>
2017-02-24 11:18:27 -0600

    Initial commit

---
+++ adinstaller.py
@@ -0,0 +1,46 @@
+#!/bin/python
+# This script is used to transfer and kick off a remote script
+# which will install packages needed for Active Directory
+# implementation on linux servers.
+
+import os, subprocess
+import re
+import sys
+import string
+import datetime
+import paramiko
+from time import strftime
+# import pexpect
+
+# Hardcode credentials
+
+username = 'bsgadmin'
+password = 'AdminBSG'
+cmd = 'uname -a'
+file_name = 'serverlist'
+host = ''
+iplist = open(file_name, 'r')
+temp = open('temp.txt', 'a')
+for data in iplist:
+        host = data
+
+# Now execute the command for each host within the loop and
+# store it in another file
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(host, username=username, password=password)
+stdin, stdout, stderr = ssh.exec_command(cmd)
+
+# VIEW THE OUTPUT ALONG WITH A TIME STAMP
+
+output = stdout.readlines()
+date = strftime("%d-%m-%Y")
+time = strftime("%H:%M:%S")
+print >>temp, date, time, host, str(output)
+
+
+temp.close()
+
+
+iplist.close()
